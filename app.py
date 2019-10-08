94601508b9024b6e3acf43e6055c595620750fc3
Colin Moore <camoore7@ncsu.edu>
2019-10-08 18:45:31 -0400

    Add endpoint to echo body

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route("/test", methods=['POST'])
+def test():
+    body = request.data.decode("utf-8")
+    print(body)
+    return body
+
 
 
 if __name__ == '__main__':
